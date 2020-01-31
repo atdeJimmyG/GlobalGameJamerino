7e68b64b020827beab7fbddc1f71c5eb19be5a0a
dayluke <dayluke01@gmail.com>
2020-01-31 19:57:07 +0000

    Added a player attack script

---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    public Weapon currentWeapon;
+    private float timeSinceLastFired;
+
+    private void Start()
+    {
+        currentWeapon.Init(currentWeapon.magazineSize, currentWeapon.magazineCount);
+    }
+
+    private void Update()
+    {
+        timeSinceLastFired += Time.deltaTime;
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            currentWeapon.Shoot(this.gameObject, timeSinceLastFired);
+            timeSinceLastFired = 0;
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            currentWeapon.Reload();
+        }
+    }
+}
