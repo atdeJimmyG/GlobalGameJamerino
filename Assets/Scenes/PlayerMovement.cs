383087265469a6a3724cb7e0860542061b5fee50
James Gill <jamesgilljames9@gmail.com>
2020-01-31 20:06:55 +0000

    Added player movement WASD/Arrow Keys

---
+++ Assets/Scenes/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody2D rb2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(moveX,moveY);
+
+        rb2d.AddForce(movement * speed);
+    }
+}
