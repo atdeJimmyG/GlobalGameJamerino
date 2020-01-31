18b2afd690b656c1cd65a288cd47c8e437b7c574
dayluke <dayluke01@gmail.com>
2020-01-31 21:53:15 +0000

    Started the weather api, and zombie spawning. Renamed scene to hopefully avoid merge conflict issues

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private int numberOfZombies = 10;
+    [SerializeField] private GameManager zombiePrefab = null;
+
+    private void Start()
+    {
+        // Spawn Zombies
+        for (int i = 0; i < numberOfZombies; i++)
+        {
+            Instantiate(zombiePrefab, Vector3.zero, Quaternion.identity);
+        }
+    }
+}
---
+++ Assets/Scripts/Weather.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Weather : MonoBehaviour
+{
+    const string apiKey = "3ad661ef210e74f92c76d79e4d7568ea";
+    const string url = "http://api.openweathermap.org/data/2.5/weather?q=";
+    private string city = "Falmouth";
+    
+    public enum WeatherStat
+    {
+        Sun,
+        Clouds,
+        Rain,
+        Storm
+    }
+    
+    private void Start()
+    {
+        StartCoroutine(GetWeather());
+    }
+
+    IEnumerator GetWeather()
+    {
+        using (UnityWebRequest uwr = UnityWebRequest.Get(url + city + "&APPID=" + apiKey))
+        {
+            yield return uwr.SendWebRequest();
+
+            Debug.Log(uwr.downloadHandler.text);
+        }
+    }
+}
