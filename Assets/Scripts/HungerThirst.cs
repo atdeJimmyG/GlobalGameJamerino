2b31275c46d69b1a8e6a9311549887a8fa539732
JS230310 <JS230310@falmouth.ac.uk>
2020-02-01 11:22:30 +0000

    Hunger and Thirst Added

---
+++ Assets/Scripts/HungerThirst.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HungerThirst : MonoBehaviour
+{
+    public float Hunger;
+    public float HungerReductRate;
+    public Slider HungerBar;
+    public float Thirst;
+    public float ThirstReductRate;
+    public Slider ThirstBar;
+
+
+    void Start()
+    {
+        while (Hunger > 0)
+        {
+            InvokeRepeating("HungerReduction", 1.0f, 1.0f);
+        }
+
+        while (Thirst > 0)
+        {
+            InvokeRepeating("ThirstReduction", 1.0f, 1.0f);
+        }
+
+    }
+
+    void HungerThirstReduction()
+    {
+        Debug.Log("Work");
+        Hunger -= HungerReductRate;
+        HungerBar.value = (Hunger);
+    }
+    void ThirstReduction()
+    {
+        Debug.Log("Work");
+        Thirst -= ThirstReductRate;
+        ThirstBar.value = (Thirst);
+    }
+}
