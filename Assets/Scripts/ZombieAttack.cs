f1e511c6c258a013dca26fbbcd1a5416059ddb48
dayluke <dayluke01@gmail.com>
2020-02-01 14:39:21 +0000

    Added a player controller and zombie attack script, so that the player can take damage. Added a 'Zombie Melee' weapon.

---
+++ Assets/Scripts/ZombieAttack.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class ZombieAttack : PlayerAttack
+{
+    private void Update()
+    {
+        timeSinceLastFired += Time.deltaTime;
+
+        if (timeSinceLastFired >= currentWeapon.fireRate)
+        {
+            currentWeapon.Shoot(this.gameObject, timeSinceLastFired);
+            timeSinceLastFired = 0;
+        }
+    }
+}
