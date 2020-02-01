b31308a6c7143c81ffe0b494689833365b2ad39c
Cole Gilbert <cg233192@falmouth.ac.uk>
2020-02-01 21:16:40 +0000

    World Generation and Building Generation

--- Assets/WorldGeneration.cs
+++ Assets/WorldGeneration.cs
@@ -12,52 +12,227 @@ public class WorldGeneration : MonoBehaviour
         FARM
     }
 
-    public GameObject[] CITY_SmallTiles;
-    public GameObject[] CITY_LargeTiles;
+    [Header("WorldTiles")]
+    public GameObject[] CITY_SmallBasicTiles;
+    public GameObject[] CITY_SmallDecorativeTiles;
+    /*public GameObject[] CITY_SmallBuildingTiles;
+    public int[] CITY_BuildingXSize;
+    public int[] CITY_BuildingYSize;
+    public GameObject[] CITY_LargeBasicTiles;
+    public GameObject[] CITY_LargeDecorativeTiles;
+    public GameObject[] CITY_LargeBuildingTiles;*/
 
-    public GameObject[] FIELD_SmallTiles;
-    public GameObject[] FIELD_LargeTiles;
+    [Space(35)]
+    public GameObject[] FIELD_SmallBasicTiles;
+    public GameObject[] FIELD_SmallDecorativeTiles;
+    /*public GameObject[] FIELD_SmallBuildingTiles;
+    public int[] FIELD_BuildingXSize;
+    public int[] FIELD_BuildingYSize;
+    public GameObject[] FIELD_LargeBasicTiles;
+    public GameObject[] FIELD_LargeDecorativeTiles;
+    public GameObject[] FIELD_LargeBuildingTiles;*/
 
-    public GameObject[] FARM_SmallTiles;
-    public GameObject[] FARM_LargeTiles;
+    [Space(35)]
+    public GameObject[] FARM_SmallBasicTiles;
+    public GameObject[] FARM_SmallDecorativeTiles;
+    /*public GameObject[] FARM_SmallBuildingTiles;
+    public int[] FARM_BuildingXSize;
+    public int[] FARM_BuildingYSize;
+    public GameObject[] FARM_LargeBasicTiles;
+    public GameObject[] FARM_LargeDecorativeTiles;
+    public GameObject[] FARM_LargeBuildingTiles;*/
 
+    [Space(50)]
     public GameObject WorldContainer;
 
-    public int WorldWidth;
-    public int WorldHeight;
+    public int CellXbyX;
+    public int AreaXbyX;
+
+    private GameObject[][,] worldMap;
+    private GameObject[,] areaMap;
+
+    
+    [Range(0f, 1f)]
+    public float BasicTileChance;
+    [Range(0f, 1f)]
+    public float DecorativeTileChance;
+    /*[Range(0f, 1f)]
+    public float BuildingTileChance;*/
 
     public AreaTypes currentArea;
 
+
+    public int seed;
+
+    void Start()
+    {
+        Random.seed = seed;
+    }
+
+#if UNITY_EDITOR
     void OnGUI()
     {
         if(GUI.Button(new Rect(10, 10, 150, 100), "Generate World"))
         {
             Debug.Log("Generating World");
+            areaMap = new GameObject[CellXbyX,CellXbyX];
+            worldMap = new GameObject[(int)Mathf.Pow(AreaXbyX,2)][,];
+            DestroyCurrentWorld();
             GenerateWorld();
+        }
+    }
+#endif
+
+    void DestroyCurrentWorld()
+    {
+        GameObject[] world = GameObject.FindGameObjectsWithTag("World");
+        for (int i = 0; i < world.Length; i++)
+        {
+            DestroyImmediate(world[i]);
         }
     }
 
     void GenerateWorld()
     {
-        Destroy(GameObject.FindGameObjectWithTag("World"));
-        GameObject container = Instantiate(WorldContainer, Vector3.zero, Quaternion.identity);
-        for(int i = 0; i < WorldHeight; i++)
+        for (int totalAreas = 0; totalAreas < Mathf.Pow(AreaXbyX,2); totalAreas++)
         {
-            for(int j = 0; j < WorldWidth; j++)
+            GameObject container = Instantiate(WorldContainer, Vector3.zero, Quaternion.identity);
+            container.name = "WorldContainer" + totalAreas;
+            int areaType = Random.Range(0, System.Enum.GetNames(typeof(AreaTypes)).Length);
+            if (areaType == 0)
             {
-                if (currentArea == AreaTypes.FIELD)
+                currentArea = AreaTypes.FIELD;
+            }
+            else if(areaType == 1)
+            {
+                currentArea = AreaTypes.FIELD;
+            }
+            else
+            {
+                currentArea = AreaTypes.FIELD;
+                //currentArea = AreaTypes.CITY;
+            }
+            for (int i = 0; i < CellXbyX; i++)
+            {
+                for (int j = 0; j < CellXbyX; j++)
                 {
-                    GameObject newTile = Instantiate(FIELD_SmallTiles[Random.Range(0,FIELD_SmallTiles.Length)], new Vector3(i, j, 0), Quaternion.identity);
-                    newTile.transform.parent = container.transform;
+                    if (currentArea == AreaTypes.FIELD)
+                    {
+                        GameObject newTile;
+                        float randomChance = Random.Range(0f, BasicTileChance + DecorativeTileChance); //+ BuildingTileChance);
+                        if (randomChance <= BasicTileChance)
+                        {
+                            int tileIndex = Random.Range(0, FIELD_SmallBasicTiles.Length);
+                            newTile = Instantiate(FIELD_SmallBasicTiles[tileIndex], new Vector3(i, j, 0), Quaternion.identity);
+                        }
+                        else if (randomChance <= DecorativeTileChance + BasicTileChance)
+                        {
+                            int tileIndex = Random.Range(0, FIELD_SmallDecorativeTiles.Length);
+                            newTile = Instantiate(FIELD_SmallDecorativeTiles[tileIndex], new Vector3(i, j, 0), Quaternion.identity);
+                        }
+                        else
+                        {
+                            int tileIndex = Random.Range(0, FIELD_SmallBasicTiles.Length);
+                            newTile = Instantiate(FIELD_SmallBasicTiles[tileIndex], new Vector3(i, j, 0), Quaternion.identity);
+                            /*int tileIndex = Random.Range(0, FIELD_SmallBuildingTiles.Length);
+                            if (i-(FIELD_BuildingXSize[tileIndex]/2) > 0 && j-(FIELD_BuildingYSize[tileIndex]/2) > 0 && i + (FIELD_BuildingXSize[tileIndex] / 2) < CellXbyX && j - (FIELD_BuildingYSize[tileIndex] / 2) < CellXbyX)
+                            {
+                                newTile = Instantiate(FIELD_SmallBuildingTiles[tileIndex], new Vector3(i, j, 0), Quaternion.identity);
+                            }
+                            else
+                            {
+                                tileIndex = Random.Range(0, FIELD_SmallBasicTiles.Length);
+                                newTile = Instantiate(FIELD_SmallBasicTiles[tileIndex], new Vector3(i, j, 0), Quaternion.identity);
+                            }*/
+                        }
+                        newTile.transform.parent = container.transform;
+                        areaMap[i, j] = newTile;
+                    }
+                    else if (currentArea == AreaTypes.FARM)
+                    {
+                        GameObject newTile;
+                        float randomChance = Random.Range(0f, BasicTileChance + DecorativeTileChance); //+ BuildingTileChance);
+                        if (randomChance <= BasicTileChance)
+                        {
+                            int tileIndex = Random.Range(0, FARM_SmallBasicTiles.Length);
+                            newTile = Instantiate(FARM_SmallBasicTiles[tileIndex], new Vector3(i, j, 0), Quaternion.identity);
+                        }
+                        else if (randomChance <= DecorativeTileChance + BasicTileChance)
+                        {
+                            int tileIndex = Random.Range(0, FARM_SmallDecorativeTiles.Length);
+                            newTile = Instantiate(FARM_SmallDecorativeTiles[tileIndex], new Vector3(i, j, 0), Quaternion.identity);
+                        }
+                        else
+                        {
+                            int tileIndex = Random.Range(0, FARM_SmallBasicTiles.Length);
+                            newTile = Instantiate(FIELD_SmallBasicTiles[tileIndex], new Vector3(i, j, 0), Quaternion.identity);
+                            /*int tileIndex = Random.Range(0, FARM_SmallBuildingTiles.Length);
+                            if (i - (FARM_BuildingXSize[tileIndex] / 2) > 0 && j - (FARM_BuildingYSize[tileIndex] / 2) > 0 && i + (FARM_BuildingXSize[tileIndex] / 2) < CellXbyX && j - (FARM_BuildingYSize[tileIndex] / 2) < CellXbyX)
+                            {
+                                newTile = Instantiate(FARM_SmallBuildingTiles[tileIndex], new Vector3(i, j, 0), Quaternion.identity);
+                            }
+                            else
+                            {
+                                tileIndex = Random.Range(0, FARM_SmallBasicTiles.Length);
+                                newTile = Instantiate(FARM_SmallBasicTiles[tileIndex], new Vector3(i, j, 0), Quaternion.identity);
+                            }*/
+                        }
+                        newTile.transform.parent = container.transform;
+                        areaMap[i, j] = newTile;
+                    }
+                    else if (currentArea == AreaTypes.CITY)
+                    {
+                        GameObject newTile;
+                        float randomChance = Random.Range(0f, BasicTileChance + DecorativeTileChance); // + BuildingTileChance);
+                        if (randomChance <= BasicTileChance)
+                        {
+                            int tileIndex = Random.Range(0, CITY_SmallBasicTiles.Length);
+                            newTile = Instantiate(CITY_SmallBasicTiles[tileIndex], new Vector3(i, j, 0), Quaternion.identity);
+                        }
+                        else if (randomChance <= DecorativeTileChance + BasicTileChance)
+                        {
+                            int tileIndex = Random.Range(0, CITY_SmallDecorativeTiles.Length);
+                            newTile = Instantiate(CITY_SmallDecorativeTiles[tileIndex], new Vector3(i, j, 0), Quaternion.identity);
+                        }
+                        else
+                        {
+                            int tileIndex = Random.Range(0, CITY_SmallBasicTiles.Length);
+                            newTile = Instantiate(CITY_SmallBasicTiles[tileIndex], new Vector3(i, j, 0), Quaternion.identity);
+                            /*int tileIndex = Random.Range(0, CITY_SmallBuildingTiles.Length);
+                            if (i - (CITY_BuildingXSize[tileIndex] / 2) > 0 && j - (CITY_BuildingYSize[tileIndex] / 2) > 0 && i + (CITY_BuildingXSize[tileIndex] / 2) < CellXbyX && j - (CITY_BuildingYSize[tileIndex] / 2) < CellXbyX)
+                            {
+                                newTile = Instantiate(CITY_SmallBuildingTiles[tileIndex], new Vector3(i, j, 0), Quaternion.identity);
+                            }
+                            else
+                            {
+                                tileIndex = Random.Range(0, CITY_SmallBasicTiles.Length);
+                                newTile = Instantiate(CITY_SmallBasicTiles[tileIndex], new Vector3(i, j, 0), Quaternion.identity);
+                            }*/
+                        }
+                        newTile.transform.parent = container.transform;
+                        areaMap[i, j] = newTile;
+                    }
+                    //Debug.Log("Tile Complete: " + (j * i));
                 }
-                else if(currentArea == AreaTypes.FARM)
-                {
-                    
-                }
-                else if(currentArea == AreaTypes.CITY)
-                {
-                    
-                }
+            }
+            worldMap[totalAreas] = areaMap;
+            //Debug.Log("Area Complete: " + (totalAreas + 1));
+        }
+        GameObject[] containers = GameObject.FindGameObjectsWithTag("World");
+        int containerNum = 0;
+        for (int x = 0; x < AreaXbyX; x++)
+        {
+            for (int y = 0; y < AreaXbyX; y++)
+            {
+                containers[containerNum].transform.position = new Vector2(x * CellXbyX, y * CellXbyX);
+                containerNum++;
+            }
+        }
+        foreach(GameObject[,] item in worldMap)
+        {
+            foreach(GameObject i in item)
+            {
+                Debug.Log(i.name);
             }
         }
     }
---
+++ Assets/WorldGenerationV2.cs
@@ -0,0 +1,180 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldGenerationV2 : MonoBehaviour
+{
+
+    [Header("WorldTiles")]
+
+    [Space(35)]
+    public GameObject[] FIELD_SmallBasicTiles;
+    public GameObject[] FIELD_SmallDecorativeTiles;
+    public GameObject[] FIELD_SmallFlowerTiles;
+
+    [Space(30)]
+    public GameObject[] HOUSES;
+
+    [Space(50)]
+    public GameObject WorldContainer;
+
+    public int WorldXbyX;
+
+    private GameObject[,] worldMap;
+
+
+    [Range(0f, 1f)]
+    public float BasicTileChance;
+    [Range(0f, 1f)]
+    public float DecorativeTileChance;
+    [Range(0f, 1f)]
+    public float FlowerTileChance;
+
+    public bool useSeed;
+    public int seed;
+    [Space(50)]
+
+
+    [Header("House Settings")]
+    public int minDistHouses;
+    private List<Vector2> existingHouses = new List<Vector2> { };
+
+    [Range(0f,5f)]
+    public float townChance;
+    [Range(0f, 5f)]
+    public float loneHouseChance;
+
+    void Start()
+    {
+        if (useSeed)
+        {
+            Random.seed = seed;
+        }
+    }
+
+#if UNITY_EDITOR
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(10, 10, 150, 100), "Generate World"))
+        {
+            Debug.Log("Generating World");
+            worldMap = new GameObject[WorldXbyX,WorldXbyX];
+            DestroyCurrentWorld();
+            GenerateWorld();
+            GenerateBuildings();
+            GenerateLoot();
+        }
+    }
+#endif
+
+    void DestroyCurrentWorld()
+    {
+        existingHouses.Clear();
+        GameObject[] world = GameObject.FindGameObjectsWithTag("World");
+        foreach(GameObject obj in world)
+        {
+            DestroyImmediate(obj);
+        }
+    }
+
+    void GenerateWorld()
+    {
+        GameObject container = Instantiate(WorldContainer, Vector3.zero, Quaternion.identity);
+        container.name = "WorldContainer";
+        for (int i = 0; i < WorldXbyX; i++)
+        {
+            for (int j = 0; j < WorldXbyX; j++)
+            {
+                GameObject newTile;
+                float randomChance = Random.Range(0f, BasicTileChance + DecorativeTileChance + FlowerTileChance);
+                if (randomChance <= BasicTileChance)
+                {
+                    int tileIndex = Random.Range(0, FIELD_SmallBasicTiles.Length);
+                    newTile = Instantiate(FIELD_SmallBasicTiles[tileIndex], new Vector3(i, j, 0), Quaternion.identity);
+                }
+                else if(randomChance <= FlowerTileChance + BasicTileChance)
+                {
+                    int tileIndex = Random.Range(0, FIELD_SmallFlowerTiles.Length);
+                    newTile = Instantiate(FIELD_SmallFlowerTiles[tileIndex], new Vector3(i, j, 0), Quaternion.identity);
+                }
+                else
+                {
+                    int tileIndex = Random.Range(0, FIELD_SmallDecorativeTiles.Length);
+                    newTile = Instantiate(FIELD_SmallDecorativeTiles[tileIndex], new Vector3(i, j, 0), Quaternion.identity);
+                }
+                newTile.transform.parent = container.transform;
+                worldMap[i, j] = newTile;
+            }
+        }
+    }
+
+    void GenerateBuildings()
+    {
+        GameObject houseContainer = Instantiate(WorldContainer, Vector3.zero, Quaternion.identity);
+        houseContainer.name = "HouseContainer";
+
+        for (int x = 5; x < WorldXbyX-5; x++)
+        {
+            float spawnChance = Random.Range(0f, townChance + loneHouseChance + 15);
+            float initialY = Random.Range(5, WorldXbyX-5);
+            float initialX = x;
+            bool spawnHouse = true;
+            foreach(Vector2 location in existingHouses)
+            {
+                if (Vector2.Distance(location, new Vector2(initialX,initialY)) < minDistHouses)
+                {
+                    spawnHouse = false;
+                }
+            }
+            if (spawnHouse)
+            {
+                if (spawnChance <= townChance)
+                {
+                    for(int i = 0; i < Random.Range(0, 12); i++)
+                    {
+                        int houseIndex = Random.Range(0, HOUSES.Length);
+                        GameObject house = Instantiate(HOUSES[houseIndex], new Vector2(initialX, initialY), Quaternion.identity);
+
+                        existingHouses.Add(new Vector2(initialX, initialY));
+
+                        if(Random.Range(0, 2) == 0)
+                        {
+                            initialX += minDistHouses;
+                        }
+                        else
+                        {
+                            initialY += minDistHouses;
+                        }
+
+                        foreach (Vector2 location in existingHouses)
+                        {
+                            if (Vector2.Distance(location, new Vector2(initialX, initialY)) < minDistHouses)
+                            {
+                                break;
+                            }
+                        }
+
+                        house.transform.parent = houseContainer.transform;
+
+                        if (initialX > WorldXbyX-5 || initialY > WorldXbyX - 5)
+                        {
+                            break;
+                        }
+                    }
+                }
+                else if (spawnChance <= loneHouseChance)
+                {
+                    int houseIndex = Random.Range(0, HOUSES.Length);
+                    GameObject house = Instantiate(HOUSES[houseIndex], new Vector2(initialX, initialY), Quaternion.identity);
+                    existingHouses.Add(new Vector2(initialX, initialY));
+                    house.transform.parent = houseContainer.transform;
+                }
+            } 
+        }
+    }
+
+    void GenerateLoot()
+    {
+        GameObject[] lootSpawns = GameObject.FindGameObjectsWithTag("LootSpawn");
+    }
+}
