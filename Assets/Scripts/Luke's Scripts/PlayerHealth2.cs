cf423f420b1b9cde908fd5f2e21f5a0167babbd6
dayluke <dayluke01@gmail.com>
2020-02-01 10:50:16 +0000

    Added Luke's stuff, cuz apparently the merge didnt add it... (like what even)

---
+++ Assets/Scripts/Luke's Scripts/PlayerHealth2.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class PlayerHealth2
+{
+    public int maxHealth;
+    public int currentHealth;
+
+    /// <summary>
+    /// Constructor method for player health.
+    /// </summary>
+    /// <param name="max"></param>
+    public PlayerHealth2(int max = 100)
+    {
+        maxHealth = max;
+        currentHealth = maxHealth;
+    }
+
+    public void Damage(int damage)
+    {
+        if (currentHealth > damage)
+        {
+            currentHealth -= damage;
+        } else
+        {
+            // Player died.
+            currentHealth = 0;
+            Debug.Log("Player died.");
+            return;
+        }
+
+        // If damage is negative, then the player will regen.
+        if (currentHealth > maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+
+        Debug.Log("Health: " + currentHealth);
+    }
+}
---
+++ Assets/Scripts/Luke's Scripts/ZombieController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class ZombieController : MonoBehaviour
+{
+    [SerializeField] private int maxHealth = 50;
+    public PlayerHealth2 health;
+
+    private void Start()
+    {
+        health = new PlayerHealth2(maxHealth);
+    }
+}
