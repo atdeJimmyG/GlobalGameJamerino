232b26413599cf013c3f68edf1c6ab298e8ccf7d
JS230310 <JS230310@falmouth.ac.uk>
2020-02-02 13:20:02 +0000

    Player Hunger and Thirst now affect the player health and stamina

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,9 +1,33 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
-public class PlayerHealth
+public class PlayerHealth : MonoBehaviour
 {
     public int maxHealth;
     public int currentHealth;
+
+    private HungerThirst PlayerThirst;
+
+    public Slider Healthbar;
+
+
+    void Start()
+    {
+        PlayerThirst = GameObject.Find("HUD").GetComponent<HungerThirst>();
+
+        InvokeRepeating("DyingofThirst", 1.0f, 1.0f);
+    }
+
+
+    void DyingofThirst()
+    {
+        if (PlayerThirst.Thirst < 20)
+        {
+            currentHealth -= 1;
+            Debug.Log("Player is dying of thirst");
+        }
+        Healthbar.value = (currentHealth);
+    }
 
     /// <summary>
     /// Constructor method for player health.
@@ -13,6 +37,10 @@ public class PlayerHealth
     {
         maxHealth = max;
         currentHealth = maxHealth;
+        if (currentHealth <= 0)
+        {
+            return;
+        }
     }
 
     public void TakeDamage(int damage)
