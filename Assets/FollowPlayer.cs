251a0d77039cb6682c4a7bad7e6f71715b881f04
Cole Gilbert <cg233192@falmouth.ac.uk>
2020-02-02 15:05:46 +0000

    Inventory Stuff and Merging UI/Player with WorldGen

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+
+    GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -10);
+    }
+}
---
+++ Assets/Scripts/Miscellaneous/PickupItems.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupItems : MonoBehaviour
+{
+
+    private Inventory inventory;
+
+    void Start()
+    {
+        inventory = GameObject.Find("HUD").GetComponent<Inventory>();
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+
+        if (other.CompareTag("Player") && Input.GetKeyDown(KeyCode.F))
+        {
+            for (int i = 0; i < inventory.Slots.Length; i++)
+            {
+                if (inventory.isFull[i] == false)
+                {
+                    inventory.isFull[i] = true;
+                    inventory.OnPickup(gameObject, i);
+                    break;
+                }
+            }
+        }
+    }
+}
